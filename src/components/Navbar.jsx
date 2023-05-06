57841576634bd62e6c6ab3ab81580ce31ee8a911
austeeee <104648080+austeeee@users.noreply.github.com>
2023-05-06 15:13:34 +0530

    Add files via upload

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import { AppBar, Box, Button, Toolbar, Typography } from '@mui/material'
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div>
+        <Box>
+            <AppBar style={{backgroundColor:'#6ef',color:'red'}}>
+                <Toolbar>
+                    <Typography varient='h6' sx={{flexGrow:1}} align='left'>App</Typography>
+                    
+                    <Button color='inherit'>Home</Button>
+                    <Button color='inherit'>Form</Button>
+                    
+                    {/* <Button color='inherit'>Table</Link></Button>
+                    <Button color='inherit'>Forms</Link></Button>
+                    <Button color='inherit'>View</Link></Button> */}
+
+
+                </Toolbar>
+            </AppBar>
+        </Box>
+    </div>
+  )
+}
+
+export default Navbar
